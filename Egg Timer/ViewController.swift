9f1c864be8de733a5fee422e10c53caa1335cb4b
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-16 02:23:28 +0400

    Code cleaned

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -11,49 +11,77 @@ import UIKit
 class ViewController: UIViewController {
     
     var timer = Timer()
+    
     var time = 210
     
     @IBOutlet weak var timerLabel: UILabel!
+    
     @IBOutlet weak var responseLabel: UILabel!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
     
     @IBAction func play(_ sender: UIBarButtonItem) {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:
+            #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        
     }
     
     @IBAction func pause(_ sender: UIBarButtonItem) {
+        
         timer.invalidate()
+        
     }
     
     @IBAction func minusTen(_ sender: UIBarButtonItem) {
+        
         if time > 10 {
+            
             time -= 10
+            
             timerLabel.text = String(time)
+            
         }
+        
     }
     
     @IBAction func plusTen(_ sender: UIBarButtonItem) {
+        
         time += 10
+        
         timerLabel.text = String(time)
+        
     }
     
     @IBAction func reset(_ sender: UIBarButtonItem) {
+        
         time = 210
+        
         timerLabel.text = String(time)
+        
     }
     
     @objc func decreaseTimer() {
+        
         if time > 0 {
+            
             time -= 1
+            
             timerLabel.text = String(time)
+            
         } else {
+            
             responseLabel.text = "Your egg boiled!"
+            
             timer.invalidate()
+            
         }
+        
     }
+    
 }
-
